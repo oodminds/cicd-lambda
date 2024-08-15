60f4a82b829a5106fde43964c2db026582ddde88
kapilkumar7 <kapil.kumar@smit.fit>
2024-08-15 13:49:34 +0530

    testing logs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello from cicd github Lambda!"),
+    body: JSON.stringify("Hello from cicd github Lambda! Report lambda"),
   };
   return response;
 };
